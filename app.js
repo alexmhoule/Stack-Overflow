6aad1e662e8eb8a30d961713ecfac64bb467143c
alexmhoule <alexmhoule@gmail.com>
2015-08-29 22:31:31 -0500

    test

---
+++ app.js
@@ -0,0 +1,168 @@
+$(document).ready( function() {
+	$('.unanswered-getter').submit( function(event){
+		// zero out results if previous search has run
+		$('.results').html('');
+		// get the value of the tags the user submitted
+		var tags = $(this).find("input[name='tags']").val();
+		getUnanswered(tags);
+	});
+
+	$('.inspiration-getter').submit( function(event){
+		$('.results').html('');
+		var answerers = $(this).find("input[name='answerers']").val();
+		getTopAnswered(answerers);
+	})
+});
+
+// this function takes the question object returned by StackOverflow 
+// and creates new result to be appended to DOM
+var showQuestion = function(question) {
+	
+	// clone our result template code
+	var result = $('.templates .question').clone();
+	
+	// Set the question properties in result
+	var questionElem = result.find('.question-text a');
+	questionElem.attr('href', question.link);
+	questionElem.text(question.title);
+
+	// set the date asked property in result
+	var asked = result.find('.asked-date');
+	var date = new Date(1000*question.creation_date);
+	asked.text(date.toString());
+
+	// set the #views for question property in result
+	var viewed = result.find('.viewed');
+	viewed.text(question.view_count);
+
+	// set some properties related to asker
+	var asker = result.find('.asker');
+	asker.html('<p>Name: <a target="_blank" href=http://stackoverflow.com/users/' + question.owner.user_id + ' >' +
+													question.owner.display_name +
+												'</a>' +
+							'</p>' +
+ 							'<p>Reputation: ' + question.owner.reputation + '</p>'
+	);
+
+	return result;
+};
+
+
+// Function for getting top answerers
+
+var showTop = function(topAnsewrers) {
+
+	// Clone my template
+	var result = $('.templates .answerer').clone();
+
+	// Get Display Name
+
+	var display = result.find('.display-text');
+	display.text(topAnsewrers.user.display_name);
+
+	// Get Reputation
+
+	var reputation = result.find('.reputation-text');
+	reputation.text(topAnsewrers.user.reputation);
+
+	// Get Accept Rate
+
+	var accepted = result.find('.accepted-text');
+	accepted.text(topAnsewrers.user.accept_rate);
+	if(topAnsewrers.user.accept_rate == null){
+		accepted.text("N / A");
+	};
+
+	// Get Profile Link
+
+	var profile = result.find('.profile-text');
+	profile.html('<p>Profile: <a target="_blank" href=http://stackoverflow.com/users/' + topAnsewrers.user.link + ' >'  + topAnsewrers.user.link + '</a></p>');
+
+	return result;
+
+};
+
+
+// this function takes the results object from StackOverflow
+// and creates info about search results to be appended to DOM
+var showSearchResults = function(query, resultNum) {
+	var results = resultNum + ' results for <strong>' + query;
+	return results;
+};
+
+// takes error string and turns it into displayable DOM element
+var showError = function(error){
+	var errorElem = $('.templates .error').clone();
+	var errorText = '<p>' + error + '</p>';
+	errorElem.append(errorText);
+};
+
+// takes a string of semi-colon separated tags to be searched
+// for on StackOverflow
+var getUnanswered = function(tags) {
+	
+	// the parameters we need to pass in our request to StackOverflow's API
+	var request = {tagged: tags,
+								site: 'stackoverflow',
+								order: 'desc',
+								sort: 'creation'};
+	
+	var result = $.ajax({
+		url: "http://api.stackexchange.com/2.2/questions/unanswered",
+		data: request,
+		dataType: "jsonp",
+		type: "GET",
+		})
+	.done(function(result){
+		var searchResults = showSearchResults(request.tagged, result.items.length);
+
+		$('.search-results').html(searchResults);
+
+		$.each(result.items, function(i, item) {
+			var question = showQuestion(item);
+			$('.results').append(question);
+		});
+	})
+	.fail(function(jqXHR, error, errorThrown){
+		var errorElem = showError(error);
+		$('.search-results').append(errorElem);
+	});
+};
+
+// Search for top answers
+
+var getTopAnswered = function(tags) {
+
+	// Parameters
+	var request = {tagged: tags,
+		site: 'stackoverflow',
+		page: 1,
+		pagesize: 25};
+
+	var result = $.ajax({
+		url: "http://api.stackexchange.com/2.2/tags/" + tags + "/top-answerers/all_time",
+		data: request,
+		dataType: "jsonp",
+		type: "GET",
+	})
+	.done(function(result){
+
+		console.log(result);
+
+		var searchResults = showSearchResults(request.tagged, result.items.length);
+
+		$('.search-results').html(searchResults);
+
+		$.each(result.items, function(i, item){
+			var topAnsewrers = showTop(item);
+			$('.results').append(topAnsewrers);
+		});
+	})
+	.fail(function(jqXHR, error, errorThrown){
+		var errorElem = showError(error);
+		$('.search-results').append(errorElem);
+	});
+};
+
+
+
